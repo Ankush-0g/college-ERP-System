bf81c1e11d1bb1820163abe0d0b4c58429bb244d
Gupta Ankushkumar Arvind <ankushgupta4747@gmail.com>
2026-05-22 00:43:52 +0530

    Update InspectDB.java

--- InspectDB.java
+++ InspectDB.java
@@ -3,7 +3,7 @@ public class InspectDB {
   public static void main(String[] args) throws Exception {
     String url = "jdbc:postgresql://localhost:5432/college";
     String user = "postgres";
-    String pass = "tan2003";
+    String pass = ur pass";
     try (Connection c = DriverManager.getConnection(url,user,pass);
          Statement st = c.createStatement()) {
       ResultSet rs = st.executeQuery("select department from hods where department not in (select name from departments);");
